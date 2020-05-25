baea3aa183ed1f0d308ad34f735e5eb601830221
Marco Elizalde <m.elizalde.torres@gmail.com>
2020-05-25 13:41:14 -0500

    (docker) update cofig docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,13 @@
 FROM node:latest
 
-COPY [".", "/usr/src/"]
+COPY ["package.json","package-lock.json","/usr/src/"]
 
 WORKDIR /usr/src
 
 RUN npm install
 
+COPY [".", "/usr/src/"]
+
 EXPOSE 3000
 
-CMD ["node", "index.js"]
+CMD ["npx","nodemon", "api/index.js"]
--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
-const port = 8080;
+const port = 3000;
 
-router.get('/', function(req,res){
-    res.send("Hola")
-  });
-  
+app.get('/', (req, res) => {
+  return res.send('Recibió un método GET');
+});
+
 
 app.listen(port, function () {
-  console.log('Example app listening on port 8080!')
+  console.log(`Servidor corriendo en el puerto ${port}`)
 })
